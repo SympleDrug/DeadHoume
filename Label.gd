b79c0273d1722b7d115bb3d39ecf209d730f9883
GHIMB <147834947+GHIMB@users.noreply.github.com>
2023-10-13 16:50:08 +0300

    Add files via upload

---
+++ Label.gd
@@ -0,0 +1,9 @@
+extends Label
+func _ready():
+	pass # Replace with function body.
+func _process(delta):
+	pass 
+
+func _on_button_pressed():
+	text="ядерка запущена"
+	pass # Replace with function body.
